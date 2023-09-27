9438f74b1207bf4fdf61183fe5ebc09fe9c7e648
Jung217 <alex24922665@gmail.com>
2023-09-27 11:49:37 +0800

    ex/ex.4

---
+++ 習題/習題4/force.py
@@ -0,0 +1,12 @@
+# x^2 - 3x + 1 = 0
+from numpy import arange
+import math
+
+def f(x) :
+    return x**2/3 - x + 1/3
+    #return math.sin(x*x+2*x)/x*x*x 
+
+for x in arange(-100, 100, 0.001):
+    if abs(f(x)) < 0.001:
+        print("x=", x, " f(x)=", f(x)) 
+
