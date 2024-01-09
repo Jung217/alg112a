cd2504e5b02aaff2e55b92dcf4ecccc608a4845f
Jung217 <alex24922665@gmail.com>
2024-01-10 01:21:20 +0800

    HW10

--- 習題/習題10/Npoly.py
+++ 習題/習題10/Npoly.py
@@ -5,25 +5,24 @@ def hillClimbing(f, init, h=0.01):
     failCount = 0
     while failCount < 10000:
         fnow = f(p)
-        p1 = p[0] + random.uniform(-h, h)
-        f1 = f(p1)
-        #p1, f1 = neighbor(f, p, h)
-        if f1 >= fnow:
+        neighbor = p.copy()
+
+        for i in range(len(neighbor)): neighbor[i] += random.uniform(-h, h)
+        f1 = f(neighbor)
+
+        if f1 < fnow:
+            if f1 <= 0 : break
             fnow = f1
-            p = p1
+            p = neighbor.copy()
             print('p=', p, 'f(p)=', fnow)
             failCount = 0
         else:
             failCount += 1
-        #if abs(fnow) < 0.001: break
+        
+        if fnow < 0.001: break
     return p, fnow
 
-#def neighbor(f, p, h=0.01):
-#    p1 = p[0] + random.uniform(-h, h)
-#    f1 = f([p1])
-#    return [p1], f1
-
 def f(x):
-    return -1 * (x[0]**5 + 1)
+    return x[0]**5 + 1
 
 print('\n', hillClimbing(f, [0.0]))
